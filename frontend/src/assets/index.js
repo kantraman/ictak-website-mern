d0091cf45440ebc7aa96960432e204e5d5dbd28b
kantraman <93331484+kantraman@users.noreply.github.com>
2022-02-24 20:43:33 +0530

    Added some academic member components

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as Faculty } from "./chalkboard-user-solid.svg";
+import { ReactComponent as Gem } from "./gem-solid.svg";
+import { ReactComponent as Star } from "./star-solid.svg";
+import { ReactComponent as Student } from "./student.svg";
+import { ReactComponent as University } from "./university.svg";
+
+export { Faculty, Gem, Star, Student, University };
---
+++ frontend/src/components/memberPage/AcademicMemberPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import TechPartners from './TechPartners';
+import AcademicBenefits from './AcademicBenefits';
+import CourseStats from './CourseStats';
+import courses from './AcademicCourseStats';
+import './memberPage.css';
+
+
+const AcademicMemberPage = () => {
+    return (
+        <>
+            <div className="memberHeading">
+                <div className="blackCoverWP">
+                    <h1>Premium Membership</h1>
+                    <p className="fs-5 mx-auto col-md-8">
+                        ICT Academy of Kerala (ICTAK) is pleased to offer its Premium Memberships to selected
+                        Engineering , Science &amp; Management Institutions and Polytechnics in the state.
+                        Through the programme, ICTAK will support its members institution’s Faculty members,
+                        Students and Staffs with quality training resources, certifications and Industry
+                        immersions on IR4.0 Technologies, and thereby enabling them to reach their Academic
+                        and Career potential.
+                    </p>
+                    <a href="#k" className="btn btn-primary btn-lg">Register</a>
+                </div>
+            </div>
+            <div className="academicBenefits">
+                <AcademicBenefits />
+            </div>
+            <div className="academicCourse">
+                <CourseStats courseStats={courses} />
+            </div>
+            <TechPartners />
+        </>
+    );
+};
+
+export default AcademicMemberPage;
