333a9d71b28e5da9e8d6738d7aeb89cdb4cc75a7
kantraman <93331484+kantraman@users.noreply.github.com>
2022-03-05 18:18:52 +0530

    Remya's update

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,115 @@
+import React from "react";
+import Logo from '../../assets/images/logo.png';
+import {
+    Button,
+    ButtonGroup,
+    Container,
+    Divider,
+    Flex,
+    IconButton,
+    Input,
+    Stack,
+    Text,
+  } from '@chakra-ui/react';
+  import { FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa';
+  import './Footer.css';
+
+const Footer = () => {
+return (
+	
+     
+      <Flex  className="footer">
+     
+        <Container as="footer" role="contentinfo">
+          <Stack
+            spacing="19"
+            direction={{ base: 'column', md: 'row' }}
+            justify="space-between"
+            py={{ base: '12', md: '16' }}
+          >
+            
+            <Stack
+            
+              direction={{ base: 'column-reverse', md: 'column', lg: 'row' }}
+              spacing={{ base: '12', md: '8' }}
+            >
+              <Stack direction="row" spacing="8">
+                <Stack spacing="4" minW="26" flex="1">
+                  <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                  ICT ACADEMY HQ
+                  </Text>
+                  <Stack spacing="3" shouldWrapChildren>
+                  <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                  G1,Ground Floor, Thejaswini Building,
+Technopark, Thiruvananthapuram,
+Kerala, India.
+<br></br>
+Mobile: 7594051437
+Phone: +91-471-2700811
+                  </Text>
+                    
+                  </Stack>
+                </Stack>
+                <Stack spacing="4" minW="36" flex="1">
+                  <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                  ICT ACADEMY HQ
+                  </Text>
+                  <Stack spacing="3" shouldWrapChildren>
+                  <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                  G1,Ground Floor, Thejaswini Building,
+Technopark, Thiruvananthapuram,
+Kerala, India.
+<br></br>
+Mobile: 7594051437
+Phone: +91-471-2700811
+                  </Text>
+                  </Stack>
+                </Stack>
+                <Stack spacing="4" minW="36" flex="1">
+                  <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                  ICT ACADEMY HQ
+                  </Text>
+                  <Stack spacing="3" shouldWrapChildren>
+                  <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                  G1,Ground Floor, Thejaswini Building,
+Technopark, Thiruvananthapuram,
+Kerala, India.
+<br></br>
+Mobile: 7594051437
+Phone: +91-471-2700811
+                  </Text>
+                  </Stack>
+                </Stack>
+                
+               
+              </Stack>
+              
+            </Stack>
+          </Stack>
+          <Divider />
+          <Stack
+            pt="8"
+            pb="12"
+            justify="space-between"
+            direction={{ base: 'column-reverse', md: 'row' }}
+            align="center"
+          >
+            <Text fontSize="sm" color="subtle">
+              &copy; {new Date().getFullYear()} ICT ACADEMY OF KERALA.
+            </Text>
+            <ButtonGroup variant="ghost">
+              <IconButton
+                as="a"
+                href="https://in.linkedin.com/company/ict-academy-of-kerala"
+                aria-label="LinkedIn"
+                icon={<FaLinkedin fontSize="1.25rem" />}
+              />
+              <IconButton as="a" href="https://www.facebook.com/ictkerala" aria-label="GitHub" icon={<FaFacebook fontSize="1.25rem" />} />
+              <IconButton as="a" href="https://www.instagram.com/ictkerala/" aria-label="Twitter" icon={<FaInstagram fontSize="1.25rem" />} />
+            </ButtonGroup>
+          </Stack>
+        </Container>
+      </Flex>
+);
+};
+export default Footer;
