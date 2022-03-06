1d796a2d5c1bea7f2b6e8c5bbe20821351e9ddbf
kantraman <93331484+kantraman@users.noreply.github.com>
2022-03-06 22:27:17 +0530

    Added routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,12 @@ import RegisterScreen from "./components/registerScreen/RegisterScreen";
 import Contact from './components/contact us/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/components/contact us/Contact.css';
+import AcademicMemberPage from './components/memberPage/AcademicMemberPage';
+import CorporateMemberPage from './components/corpMemPage/CorporateMemberPage';
+import Partnership from './components/partnership/Partnership';
+import CorporateMember from './components/membership/CorporateMember';
+import PremiumMember from './components/membership/PremiumMember';
+import DateRange from './components/dateRange/DateRange';
 
 
 import { Mainpage } from "./components/Mainpage";
@@ -19,13 +25,22 @@ function App() {
     <BrowserRouter>
       <main>
         <Routes>
-        <Route path="/" element={<Mainpage />} />
-          <Route path="/cources" element={<Cources />} />
-          <Route path="/CreateCource" element={<CreateCource />} />
-          <Route path="/cource/:id" element={<UpdateCource />} />
-          <Route path="/singlecourse/:id" element={<SingleCourse />} />
-          <Route path="/registerScreen" element={<RegisterScreen />} />
-          <Route path="/Contact" element={<Contact />} />
+            <Route path="/" element={<Mainpage />} />
+            <Route path="/cources" element={<Cources />} />
+            <Route path="/CreateCource" element={<CreateCource />} />
+            <Route path="/cource/:id" element={<UpdateCource />} />
+            <Route path="/singlecourse/:id" element={<SingleCourse />} />
+            <Route path="/registerScreen" element={<RegisterScreen />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/academic-member" element={<AcademicMemberPage />} />
+            <Route path="/corporate-member" element={<CorporateMemberPage />} />
+            <Route path="/academic-member-apply" element={<PremiumMember />} />
+            <Route path="/corporate-member-apply" element={<CorporateMember />} />
+            <Route path="/partnership" element={<Partnership />} />
+            <Route path="/export-academic" element={<DateRange type={1} />} />
+            <Route path="/export-corporate" element={<DateRange type={2} />} />
+            <Route path="/export-partnership" element={<DateRange type={3} />} />
+            <Route path="/export-messages" element={<DateRange type={4} />} />
         </Routes>
       </main>
     </BrowserRouter>
