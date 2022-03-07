b954bb8a9d786577bdb8ee88eb2110fb9348e8ee
swapnaliss <swapnalisbhargude123@gmail.com>
2022-03-07 12:00:46 +0530

    fixed provider issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,11 @@ import {Provider} from "react-redux";
 import { ChakraProvider } from '@chakra-ui/react'
 import store from "./store";
 ReactDOM.render(
-  <ChakraProvider store = {store}>
-    <App />
+  <ChakraProvider store={store}>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ChakraProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
