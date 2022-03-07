ae24618c9ab81f965f424a06bd6f674fc216ce6c
kantraman <93331484+kantraman@users.noreply.github.com>
2022-03-07 22:30:46 +0530

    Added dashsearchbox

--- frontend/src/components/Dashboard/dashhome/Dashhome.js
+++ frontend/src/components/Dashboard/dashhome/Dashhome.js
@@ -4,10 +4,12 @@ import { userData } from "../dashboard/dummyData";
 import WidgetSm from "../widgetSm/WidgetSm";
 import FeaturedInfo from '../featuredInfo/FeaturedInfo'
 import  Chart  from '../chart/Chart';
+import Dashsearch from '../dashSearch/Dashsearch';
 
 function Dashhome(props) {
     return (
-        <div className="Dashhome">
+      <div className="Dashhome">
+        <Dashsearch />
       <FeaturedInfo />
       <div className='Charts'>
       <Chart data={userData} />
