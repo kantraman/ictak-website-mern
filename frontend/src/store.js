d1832e817d5c0417e0b5d0571d04f8ca5c19c179
kantraman <93331484+kantraman@users.noreply.github.com>
2022-03-12 08:00:17 +0530

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,13 @@ import { createStore , applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 import { courceCreateReducer, courceDeleteReducer, courceListReducers, courceUpdateReducer } from "./reducers/courceReducers";
-
+import {courseUserRegisterReducer} from "./reducers/userReducers"
 const reducer = combineReducers({
-    courceList : courceListReducers,
-    courceCreate : courceCreateReducer,
-    courceUpdate : courceUpdateReducer, 
-    courceDelete : courceDeleteReducer,
+  courceList: courceListReducers,
+  courceCreate: courceCreateReducer,
+  courceUpdate: courceUpdateReducer,
+  courceDelete: courceDeleteReducer,
+  courseUserRegisterReducer:courseUserRegisterReducer,
 });
 
 const initialState = {};
